bee6ff6ca31d7c13670e769d3589d7cd635b3259
xgravebornx <kmpo83@gmail.com>
2013-05-22 17:25:35 -0300

    Create TileEntityGrave1.java

---
+++ TileEntityGrave1.java
@@ -0,0 +1,14 @@
+package mods.tutorial.common;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.INetworkManager;
+import net.minecraft.network.packet.Packet;
+import net.minecraft.network.packet.Packet132TileEntityData;
+import net.minecraft.tileentity.TileEntity;
+
+public class TileEntityGrave1 extends TileEntity {
+
+  public static final TileEntityGrave1 tileentity = null;
+
+	
+}
