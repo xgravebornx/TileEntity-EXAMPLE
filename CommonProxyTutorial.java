9cc07da964a1faac9f2283d9ce37dec9900905c2
xgravebornx <kmpo83@gmail.com>
2013-05-22 17:19:00 -0300

    Update and rename erger to CommonProxyTutorial.java

---
+++ CommonProxyTutorial.java
@@ -0,0 +1,12 @@
+package mods.tutorial.client;
+
+import mods.tutorial.common.CommonProxyTutorial;
+
+public class ClientProxyTutorial extends CommonProxyTutorial{
+  
+	@Override
+	public void RegisterRenders(){
+		
+	}
+
+}
