9f9566ff8ceed05baa1c61a74f1550c9473bc59b
xgravebornx <kmpo83@gmail.com>
2013-09-09 15:09:30 -0400

    Update Grave1Block.java

--- Grave1Block.java
+++ Grave1Block.java
@@ -1,4 +1,4 @@
-package mods.tutorial.common;
+package mods.cosmetica.common;
 
 import static net.minecraftforge.common.ForgeDirection.EAST;
 import static net.minecraftforge.common.ForgeDirection.NORTH;
@@ -13,8 +13,10 @@ import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityFurnace;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
@@ -22,59 +24,83 @@ import net.minecraft.world.World;
 
 
 public class Grave1Block extends BlockContainer{
+	
+	
 
-/**
+	/**
      * Called whenever the block is added into the world. Args: world, x, y, z
      */
-public void onBlockAdded(World par1World, int par2, int par3, int par4)
-{
-     super.onBlockAdded(par1World, par2, par3, par4);
-     this.setDefaultDirection(par1World, par2, par3, par4);
-}
-/**
-     * set a blocks direction
-     */
-private void setDefaultDirection(World par1World, int par2, int par3, int par4)
-{
-     if (!par1World.isRemote)
-     {
-             int l = par1World.getBlockId(par2, par3, par4 - 1);
-             int i1 = par1World.getBlockId(par2, par3, par4 + 1);
-             int j1 = par1World.getBlockId(par2 - 1, par3, par4);
-             int k1 = par1World.getBlockId(par2 + 1, par3, par4);
-             byte b0 = 3;
-             if (Block.opaqueCubeLookup[l] && !Block.opaqueCubeLookup[i1])
-             {
-                     b0 = 3;
-             }
-             if (Block.opaqueCubeLookup[i1] && !Block.opaqueCubeLookup[l])
-             {
-                     b0 = 2;
-             }
-             if (Block.opaqueCubeLookup[j1] && !Block.opaqueCubeLookup[k1])
-             {
-                     b0 = 5;
-             }
-             if (Block.opaqueCubeLookup[k1] && !Block.opaqueCubeLookup[j1])
-             {
-                     b0 = 4;
-             }
-             par1World.setBlockMetadataWithNotify(par2, par3, par4, b0, 2);
-     }
-}
-@Override
-public void onBlockPlacedBy(World world, int x, int y, int z, EntityLiving player, ItemStack itemStack) {
-    replaceGround(world, x, y - 1, z);
-
-    int direction = MathHelper.floor_float(player.rotationYaw);
-    if (direction < 0) {
-        direction = 360 + direction;
+    public void onBlockAdded(World par1World, int par2, int par3, int par4)
+    {
+        super.onBlockAdded(par1World, par2, par3, par4);
+        this.setDefaultDirection(par1World, par2, par3, par4);
     }
 
-    int metadata = getMetadataBasedOnRotation(direction);
-    world.setBlockMetadataWithNotify(x, y, z, metadata, 2);
+    /**
+     * set a blocks direction
+     */
+    private void setDefaultDirection(World par1World, int par2, int par3, int par4)
+    {
+        if (!par1World.isRemote)
+        {
+            int l = par1World.getBlockId(par2, par3, par4 - 1);
+            int i1 = par1World.getBlockId(par2, par3, par4 + 1);
+            int j1 = par1World.getBlockId(par2 - 1, par3, par4);
+            int k1 = par1World.getBlockId(par2 + 1, par3, par4);
+            byte b0 = 3;
 
-}
+            if (Block.opaqueCubeLookup[l] && !Block.opaqueCubeLookup[i1])
+            {
+                b0 = 3;
+            }
+
+            if (Block.opaqueCubeLookup[i1] && !Block.opaqueCubeLookup[l])
+            {
+                b0 = 2;
+            }
+
+            if (Block.opaqueCubeLookup[j1] && !Block.opaqueCubeLookup[k1])
+            {
+                b0 = 5;
+            }
+
+            if (Block.opaqueCubeLookup[k1] && !Block.opaqueCubeLookup[j1])
+            {
+                b0 = 4;
+            }
+
+            par1World.setBlockMetadataWithNotify(par2, par3, par4, b0, 2);
+        }
+    }
+    public void onBlockPlacedBy(World par1World, int par2, int par3, int par4, EntityLivingBase par5EntityLivingBase, ItemStack par6ItemStack)
+    {
+        int l = MathHelper.floor_double((double)(par5EntityLivingBase.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+
+        if (l == 0)
+        {
+            par1World.setBlockMetadataWithNotify(par2, par3, par4, 2, 2);
+        }
+
+        if (l == 1)
+        {
+            par1World.setBlockMetadataWithNotify(par2, par3, par4, 5, 2);
+        }
+
+        if (l == 2)
+        {
+            par1World.setBlockMetadataWithNotify(par2, par3, par4, 3, 2);
+        }
+
+        if (l == 3)
+        {
+            par1World.setBlockMetadataWithNotify(par2, par3, par4, 4, 2);
+        }
+
+        if (par6ItemStack.hasDisplayName())
+        {
+            ((TileEntityFurnace)par1World.getBlockTileEntity(par2, par3, par4)).setGuiDisplayName(par6ItemStack.getDisplayName());
+        }
+    }
 
 private static int getMetadataBasedOnRotation(int rotation) {
     if (rotation >= 315 || rotation < 45) {
@@ -135,6 +161,8 @@ private static int getMetadataBasedOnRotation(int rotation) {
 	        }
 	    
 	    }
+	    
+	    protected boolean bFull3D = false;
 	    
 		@Override
 		public TileEntity createNewTileEntity(World world) {
